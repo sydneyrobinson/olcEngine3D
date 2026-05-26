2ee682b678ee837562d060b81f8456000113b2e3
sydney <sydneyrobinson.2510@gmail.com>
2026-05-25 20:48:12 -0400

    Simple cube rotation with simple lighting complete.

--- olcEngine3D.cpp
+++ olcEngine3D.cpp
@@ -253,11 +253,11 @@ public:
                     triProjected.p[2].x, triProjected.p[2].y,
                     triProjected.sym, triProjected.col);
 
-                /*DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
                     triProjected.p[1].x, triProjected.p[1].y,
                     triProjected.p[2].x, triProjected.p[2].y,
-                    PIXEL_SOLID, FG_WHITE);
-                */
+                    PIXEL_SOLID, FG_BLACK);
+                
             }
             
                
